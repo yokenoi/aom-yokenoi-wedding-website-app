b8f6b5385035e235b1651d362c03bcc4665b70be
Yokenoi <yokenoi_@hotmail.com>
2024-10-24 19:44:39 +0700

    Add header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,30 @@
 <script>
-    import UnderDevelopment from "./UnderDevelopment.svelte";
+	// import { redirect, redirect } from "@sveltejs/kit";
+
+    // import UnderDevelopment from "./UnderDevelopment.svelte";
+    import Header from "./Header.svelte";
 </script>
 
 <svelte:head>
     <title>Aom & Yoke Wedding</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin=true>
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet">
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            height: 100vh;
+        }
+
+        body * {
+            margin: 0;
+            padding: 0;
+        }
+    </style>
 </svelte:head>
 
-<UnderDevelopment />
+<div style="margin: 0 auto; padding: 0; max-width: 480px;">
+    <Header />
+</div>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,72 @@
+<script>
+    import img from '$lib/images/header/home_bg.png'
+    import logo from '$lib/images/header/logo.svg'
+    import banner from '$lib/images/header/banner.svg'
+</script>
+
+<header>
+    <img src="{img}" alt="bg_img">
+    <div class="container">
+        <nav>
+            <img class="logo" src="{logo}" alt="logo">
+        </nav>
+        <img src="{banner}" alt="" class="date">
+        <p>#YokeFoundHisAom</p>
+            
+    </div>
+        
+</header>
+
+<style>
+    .container {
+        position: absolute;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        max-width: 480px;
+        max-height: 1038px;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        flex-direction: column;
+    }
+    
+    header {
+        width: 100%;
+        height: 100%;
+        max-height: 1038px;
+        & > img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+    
+    .date {
+        position: relative;
+        width: 58%;
+        bottom: 20%;
+    }
+
+    nav {
+        width: 100%;
+        max-width: 480px;
+        position: relative;
+        top: 5%;
+        /* background-color: red; */
+        text-align: center;
+    }
+
+    p {
+        font-family: "Cormorant", serif;
+        color: white;
+        font-size: 18px;
+        font-style: italic;
+        position: relative;
+        bottom: 5%;
+    }
+
+
+
+</style>

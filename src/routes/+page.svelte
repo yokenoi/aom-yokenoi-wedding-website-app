ecad25890fd69b24ba9e05666f0b32c6fae84b65
Yokenoi <yokenoi_@hotmail.com>
2024-11-05 12:34:29 +0700

    Add Metadata

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script>
     import Header from "./Header.svelte";
     import Card from "./Card.svelte";
+    import metaImgae from '$lib/images/meta_image.webp'
 </script>
 
 <svelte:head>
     <title>Aom & Yoke Wedding</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin=true>
+    <meta property="og:image" content={metaImgae}>
     <link href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,300..700;1,300..700&family=Noto+Serif:ital,wght@0,100..900;1,100..900&family=Poppins:wght@300&display=swap" rel="stylesheet">
     <style>
         body {

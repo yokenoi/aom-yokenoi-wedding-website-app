df5777a5ca2b10d164aecd47ffef050422c2ee6d
Yokenoi <yokenoi_@hotmail.com>
2024-11-24 13:11:43 +0700

    Fix Chiang Mai Gallery

---
+++ src/routes/gallery/chiangmai/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+    const imageModules = import.meta.glob("$lib/images/gallery/chiangmai/*.webp");
+    // const imageUrls = []
+    // for (const modulePath in imageModules) {
+    //     imageModules[modulePath]().then((imageUrl) => {
+    //         console.log(modulePath, imageUrl);
+    //         imageUrls.push(imageUrl)
+    //     });
+    // }
+</script>
+
+<div class="container">
+    {#each Object.values(imageModules) as importImage}
+        {#await importImage() then { default: imgUrl }}
+            <img src="{imgUrl}" alt="{imgUrl}">
+        {/await}
+    {/each}
+</div>
+
+
+<style>
+    .container {
+        width: 95%;
+        max-width: 480px;
+        margin: 10px auto;
+        row-gap: 10px;
+        display: grid;
+
+        justify-content: center;
+    }
+
+    img {
+        width: 100%;
+    }
+</style>
